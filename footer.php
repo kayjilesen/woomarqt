26eec09f754e797dd2c5ee31a9564f7496db0635
Jarno Peeters <info@insideweb.nl>
2020-11-09 14:53:31 +0100

    Update footer.php

--- footer.php
+++ footer.php
@@ -4,20 +4,29 @@
  */
 
 //  Define Variables
-$preset = get_field('footer_preset', 'option');
-$copyright = get_field('copyright', 'option');
-$betaalmethodes = get_field('betaalmethodes', 'option');
-$vervoerders = get_field('vervoerders', 'option');
+$toon_usps = get_field('toon_usps', 'option');
+$usps_overnemen = get_field('usps_overnemen', 'option');
+$toon_widgets = get_field('toon_widgets', 'option');
+$toon_copyright = get_field('toon_copyright', 'option');
+$toon_betaalmethodes = get_field('toon_betaalmethodes', 'option');
+$toon_vervoerders = get_field('toon_vervoerders', 'option');
+$toon_contact = get_field('toon_contact', 'option');
+$toon_klantenservice = get_field('toon_klantenservice', 'option');
 
-if ($preset === 'Footer 1') { ?>
-    <section class="footer container bg-gray-200">
-        <p>Test</p>
+if ($toon_widgets) { ?>
+    <section class="footer container bg-gray-200 grid-cols-4 gap-8 grid-flow-row">
+        <?php if ($toon_contact) { ?>
+            <div class="col-auto">
+                <p>Toon contact</p>
+            </div>
+        <?php } ?>
+        <?php if ($toon_klantenservice) { ?>
+            <div class="col-auto">
+                <p>Toon klantenservice</p>
+            </div>
+        <?php } ?>
     </section>
-<?php } else if ($preset === 'Footer 2') {
-    print_r('Footer 2');
-}
-
-?>
+<?php } ?>
 
 
 
