88c7d99b32a94b205a89b2d5af4a70f8273b8325
Jarno Peeters <info@insideweb.nl>
2020-11-09 13:01:30 +0100

    header.php en footer.php

--- header.php
+++ header.php
@@ -24,10 +24,6 @@ $menuitems = wp_get_nav_menu_items( $menu->term_id, array( 'order' => 'DESC' ) )
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
-    <!-- Load Heading Font -->
-    <link href="https://fonts.googleapis.com/css2?family=<?php get_field('heading_font', 'option') ?>&display=swap" rel="stylesheet">
-    <!-- Load Body Font -->
-    <link href="https://fonts.googleapis.com/css2?family=<?php get_field('body_font', 'option') ?>&display=swap" rel="stylesheet">
     <!-- Load Tailwind CSS CDN -->
     <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
 
