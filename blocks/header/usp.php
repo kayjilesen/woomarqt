dfb0e80a788e65dcae30c8f3ebec917202396581
Kay Jilesen <k.jilesen@student.fontys.nl>
2020-11-09 14:31:56 +0100

    Menu Order Options

--- blocks/header/usp.php
+++ blocks/header/usp.php
@@ -1,9 +1,9 @@
 <div id="usp" class="navigation-bar lightColor">
-    <div class="innerWrapper max-w-screen-xl flex py-3 mx-auto">
+    <div class="innerWrapper max-w-screen-xl flex py-3 px-4 mx-auto">
         <?php   
         if(!empty($uspbar['usps'])) :
             foreach($uspbar['usps'] as $usp){
-                echo '<div class="uspBlock mr-3"><i class="mr-2 ' . $usp['icon'] . '"></i>' . $usp['usp'] . '</div>';
+                echo '<div class="uspBlock text-sm mr-8"><i class="mr-2 ' . $usp['icon'] . '"></i>' . $usp['usp'] . '</div>';
             }
         endif;
         ?>
