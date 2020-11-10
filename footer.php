c732c9552e033bfd723e489589bec15d8f09ec85
Jarno Peeters <info@insideweb.nl>
2020-11-10 12:06:26 +0100

    Add Payment Method

--- footer.php
+++ footer.php
@@ -9,36 +9,48 @@ $usps_overnemen = get_field('usps_overnemen', 'option');
 $toon_widgets = get_field('toon_widgets', 'option');
 $toon_copyright = get_field('toon_copyright', 'option');
 $toon_betaalmethodes = get_field('toon_betaalmethodes', 'option');
+$betaalmethodes = get_field('betaalmethodes', 'option');
 $toon_vervoerders = get_field('toon_vervoerders', 'option');
 $toon_contact = get_field('toon_contact', 'option');
 $toon_klantenservice = get_field('toon_klantenservice', 'option');
 
 ?>
 
-<footer>
-    <section id="footer" class="w-full bg-gray-200">
-    <?php if ($toon_widgets) { ?>
-        <div id="footer-widgets" class="py-10 mx-auto max-w-screen-sm max-w-screen-md max-w-screen-lg max-w-screen-xl grid grid-cols-4">
-            <?php if ($toon_contact) { ?>
-                <div class="col-auto">
-                    <h5>Contact</h5>
-                </div>
-            <?php } ?>
-            <?php if ($toon_klantenservice) { ?>
-                <div class="col-auto">
-                    <h5>Klantenservice</h5>
-                </div>
-            <?php } ?>
-        </div>
+<footer id="footer" class="w-full">
+    <section id="footer-widgets" class="w-full">
+        <?php if ($toon_widgets) { ?>
+            <div class="py-10 mx-auto max-w-screen-sm max-w-screen-md max-w-screen-lg max-w-screen-xl grid grid-cols-4">
+                <?php if ($toon_contact) { ?>
+                    <div class="col-auto">
+                        <h5>Contact</h5>
+                    </div>
+                <?php } ?>
+                <?php if ($toon_klantenservice) { ?>
+                    <div class="col-auto">
+                        <h5>Klantenservice</h5>
+                    </div>
+                <?php } ?>
+            </div>
         <?php } ?>
+    </section>
+
+    <section id="footer-copyright" class="w-full">
         <?php if ($toon_copyright) { ?>
-        <div id="footer-copyright" class="py-10 mx-auto max-w-screen-sm max-w-screen-md max-w-screen-lg max-w-screen-xl grid">
-            <?php if ($toon_contact) { ?>
-                <div class="col-auto">
-                    <p>Copyright &copy; <?php echo date("Y") . " " . get_bloginfo('name') . " | " . "powered by <a href='#'>WooMarqt</a>" ?></p>
-                </div>
-            <?php } ?>
-        </div>
+            <div class="py-6 mx-auto max-w-screen-sm max-w-screen-md max-w-screen-lg max-w-screen-xl grid grid-cols-2 text-sm">
+                <?php if ($toon_copyright) { ?>
+                    <div class="col-auto">
+                        <p>Copyright &copy; <?php echo date("Y") . " " . get_bloginfo('name') . " | " . "powered by <a href='#'>WooMarqt</a>" ?></p>
+                    </div>
+                <?php } ?>
+
+                <?php if ($toon_betaalmethodes) { ?>
+                    <div class="col-auto">
+                        <?php if ($betaalmethodes['ideal']) { ?>
+                            <img src="/wp-content/themes/woomarqt/assets/img/payment-methods/ideal.svg">
+                        <?php } ?>
+                    </div>
+                <?php } ?>
+            </div>
         <?php } ?>
     </section>
 </footer>
--- functions/css.php
+++ functions/css.php
@@ -7,7 +7,8 @@ function getCSSContent(){
     $subbar = get_field('topbar', 'option');
     $uspbar = get_field('usp', 'option');
 
-    $content = '/*Generated CSS file based on Theme Settings*/';
+    $content = '/* Generated CSS file based on Theme Settings */';
+
     // Header CSS
     $content .= (!empty($huisstijl['primary_color']) ? '.priBackgroundColor{background-color:' . $huisstijl['primary_color'] . ';}.priTextColor{color:' . $huisstijl['primary_color'] . ';}' : '');
     $content .= (!empty($huisstijl['secondary_color']) ? '.secBackgroundColor{background-color:' . $huisstijl['secondary_color'] . ';}.secTextColor{color:' . $huisstijl['secondary_color'] . ';}' : '');
@@ -15,7 +16,10 @@ function getCSSContent(){
     $content .= ($header['custom_volgorde'] === 'lmsi' ? '#head.lmsi .logoCol{order: 1;}#head.lmsi  .menuCol{order: 2;}#head.lmsi .searchCol{order: 3;}' : ($header['custom_volgorde'] === 'lsmi' ? '#head.lsmi .logoCol{order: 1;}#head.lsmi .menuCol{order: 3;}#head.lsmi .searchCol{order: 2;}' : '' ));
     if($topbar['show_topbar']) $content .= '#top{background-color:' . $topbar['styling']['background_color'] . ';color:' . $topbar['styling']['text_color'] . ';}#top svg{color: ' . $topbar['styling']['text_color'] . ';}';
     if($uspbar['show_usps']) $content .= '#usp{background-color:' . $uspbar['styling']['background_color'] . ';color:' . $uspbar['styling']['text_color'] . '}#usp svg{color: ' . $uspbar['icon_color'] . ';}';
+
     // Footer CSS
-    $content .= '#footer{background-color:' . get_field('achtergrondkleur', 'option') . ';}';
+    $content .= '#footer-widgets{background-color:' . get_field('widgets_achtergrondkleur', 'option') . ';}';
+    $content .= '#footer-copyright{background-color:' . get_field('copyright_achtergrondkleur', 'option') . ';}';
+    
     return $content;
 }
