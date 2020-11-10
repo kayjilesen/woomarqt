ae1dabc0db16e4b73aa5a8f028e3ff48696b8f90
Jarno Peeters <info@insideweb.nl>
2020-11-10 17:18:40 +0100

    Update footer.php

--- footer.php
+++ footer.php
@@ -16,7 +16,7 @@ $footer_carriers            =   get_field('footer_carriers', 'option');
     <?php if ($footer_settings['show_usps']) { ?>
 
         <section id="footer-usps" class="w-full">
-            <div class="py8 mx-auto max-w-screen-sm max-w-screen-md max-w-screen-lg max-w-screen-xl">
+            <div class="py-8 mx-auto max-w-screen-sm max-w-screen-md max-w-screen-lg max-w-screen-xl grid">
                 <p>Hier komen USP's</p>
             </div>
         </section>
@@ -28,14 +28,24 @@ $footer_carriers            =   get_field('footer_carriers', 'option');
         <section id="footer-widgets" class="w-full">
             <div class="py-10 mx-auto max-w-screen-sm max-w-screen-md max-w-screen-lg max-w-screen-xl grid grid-cols-4">
                 <?php if ($footer_widgets['show_contact']) { ?>
-                    <div class="col-auto">
+                    <div class="">
                         <h5>Contact</h5>
                     </div>
                 <?php } ?>
 
                 <?php if ($footer_widgets['show_service']) { ?>
-                    <div class="col-auto">
+                    <div class="">
                         <h5>Klantenservice</h5>
+                        <?php
+                            // print_r($footer_widgets['service_menu']);
+                            if ($footer_widgets['service_menu']) :
+                                ?> <ul> <?php
+                                    foreach ($footer_widgets['service_menu'] as $page) {
+                                        print_r($page['page']);
+                                    }
+                                ?> </ul> <?php
+                            endif;
+                        ?>
                     </div>
                 <?php } ?>
             </div>
@@ -47,21 +57,28 @@ $footer_carriers            =   get_field('footer_carriers', 'option');
 
         <section id="footer-copyright" class="w-full">
             <div class="py-6 mx-auto max-w-screen-sm max-w-screen-md max-w-screen-lg max-w-screen-xl grid grid-cols-2 text-sm">
-                <div class="col-auto">
+                <div class="self-center">
                     <p>Copyright &copy; <?php echo date("Y") . " " . get_bloginfo('name') . " | " . "powered by <a href='#'>WooMarqt</a>" ?></p>
                 </div>
 
                 <?php if ($footer_settings['show_payment_methods']) { ?>
+                    <div class="grid grid-flow-col gap-3">
                         <?php if ($footer_payment_methods['payment_methods']['ideal']) { ?>
-                            <svg height="35">
-                                <image href="https://woomarqt.nl/wp-content/themes/woomarqt/assets/img/payment-methods/ideal.svg" height="35" />
+                            <svg height="30">
+                                <image href="https://woomarqt.nl/wp-content/themes/woomarqt/assets/img/payment-methods/ideal.svg" height="30" />
                             </svg>
                         <?php } ?>
                         <?php if ($footer_payment_methods['payment_methods']['paypal']) { ?>
-                            <svg height="35">
-                                <image href="https://woomarqt.nl/wp-content/themes/woomarqt/assets/img/payment-methods/paypal.svg" height="35" />
+                            <svg height="30">
+                                <image href="https://woomarqt.nl/wp-content/themes/woomarqt/assets/img/payment-methods/paypal.svg" height="30" />
                             </svg>
                         <?php } ?>
+                        <?php if ($footer_payment_methods['payment_methods']['creditcard']) { ?>
+                            <svg height="30">
+                                <image href="https://woomarqt.nl/wp-content/themes/woomarqt/assets/img/payment-methods/creditcard.svg" height="30" />
+                            </svg>
+                        <?php } ?>
+                    </div>
                 <?php } ?>
             </div>
         </section>
