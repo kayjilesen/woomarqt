6327bbd5f9dc3b8eb57fc68b4448447f3f859f11
Kay Jilesen <k.jilesen@student.fontys.nl>
2020-11-17 16:53:23 +0100

    Mobile Menu Full Screen

--- blocks/header/head.php
+++ blocks/header/head.php
@@ -12,30 +12,33 @@
             <div class="md:flex items-center justify-start space-x-8 md:flex-1 lg:w-3/12 logoCol">
                 <?php  if(!empty($huisstijl['logo'])) echo '<a href="' . home_url() . '">' . ($header['kleur_logo'] ? '<img src="' . $huisstijl['logo']['url'] . '" alt="' . $huisstijl['logo']['alt'] . '">' : '<img src="' . $huisstijl['logo_light']['url'] . '" alt="' . $huisstijl['logo_light']['alt'] . '">' ) . '</a>'; ?>
             </div>
-            <nav class="hidden md:flex space-x-10 menuCol lg:w-6/12 justify-center">
-                <?php
-                    for($i = 0; $i < count($menuitems); $i++){
-                        if($menuitems[$i]->menu_item_parent == 0){
-                            $firstChild = true;
-                            echo ($firstParent == true ? $firstParent = false : '</div>');
-                            echo '<div class="nav-item ' . ($i < (count($menuitems)-1)?($menuitems[$i + 1]->menu_item_parent != 0 ? 'dropdown' : ''):'') . ($menuitems[$i]->title == 'Contact' ? 'contact ' : '') . (get_the_title() ===  $menuitems[$i]->title ? 'active' : '') . '">';
-                                echo '<a title="' . get_bloginfo( ) . ' Pagina ' . $menuitems[$i]->title . '" href="' .  $menuitems[$i]->url . '" class="nav-link title">';
-                                    echo '<h3>' .  $menuitems[$i]->title . ($i < (count($menuitems)-1)?($menuitems[$i + 1]->menu_item_parent != 0 ? '' : ''): '') . '</h3>';
-                                echo '</a>';
-                        } else {
-                            if($firstChild == true){
-                                $firstChild = false;
-                                echo '<div class="dropdown-menu">';
+            <div class="md:flex mobileNavContainer">
+                <nav class="hidden md:flex space-x-10 menuCol lg:w-6/12 justify-center">
+                    <?php
+                        for($i = 0; $i < count($menuitems); $i++){
+                            if($menuitems[$i]->menu_item_parent == 0){
+                                $firstChild = true;
+                                echo ($firstParent == true ? $firstParent = false : '</div>');
+                                echo '<div class="nav-item ' . ($i < (count($menuitems)-1)?($menuitems[$i + 1]->menu_item_parent != 0 ? 'dropdown' : ''):'') . ($menuitems[$i]->title == 'Contact' ? 'contact ' : '') . (get_the_title() ===  $menuitems[$i]->title ? 'active' : '') . '">';
+                                    echo '<a title="' . get_bloginfo( ) . ' Pagina ' . $menuitems[$i]->title . '" href="' .  $menuitems[$i]->url . '" class="nav-link flex items-center title">';
+                                        echo '<h3>' .  $menuitems[$i]->title . ($i < (count($menuitems)-1)?($menuitems[$i + 1]->menu_item_parent != 0 ? '' : ''): '') . '</h3>';
+                                    echo '</a>';
+                                    echo ($menuitems[$i + 1]->menu_item_parent != 0 ? '<div class="openDropdown flex justify-center items-center">></div>' : '');
+                            } else {
+                                if($firstChild == true){
+                                    $firstChild = false;
+                                    echo '<div class="dropdown-menu">';
+                                }
+                                echo '<a class="dropdown-item" title="' . get_bloginfo(  ) . ' Pagina ' . $menuitems[$i]->title . '" href="' . $menuitems[$i]->url . '"><h6>' . $menuitems[$i]->title . '</h6></a>';
+                                echo (!empty($menuitems[$i + 1]) && $menuitems[$i + 1]->menu_item_parent == 0 ? '</div>' : '');
                             }
-                            echo '<a class="dropdown-item" title="' . get_bloginfo(  ) . ' Pagina ' . $menuitems[$i]->title . '" href="' . $menuitems[$i]->url . '"><h6>' . $menuitems[$i]->title . '</h6></a>';
-                            echo (!empty($menuitems[$i + 1]) && $menuitems[$i + 1]->menu_item_parent == 0 ? '</div>' : '');
                         }
-                    }
-                    echo '</div>';
-                ?> 
-            </nav>
-            <div class="md:flex items-center justify-end space-x-8 md:flex-1 lg:w-3/12 searchCol">
-                <?php if($header['show_searchbar']) echo '<input type="text" name="search" placeholder="" value="">'; ?>
+                        echo '</div>';
+                    ?> 
+                </nav>
+                <div class="md:flex items-center justify-end space-x-8 md:flex-1 lg:w-3/12 searchCol">
+                    <?php if($header['show_searchbar']) echo '<input type="text" name="search" placeholder="" value="">'; ?>
+                </div>
             </div>
             <div class="flex lg:hidden mobileNav">
                 <button class="navbar-toggler" id="toggleMenu" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
